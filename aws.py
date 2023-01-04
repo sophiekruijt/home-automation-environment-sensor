d90a141b68bbe1a270e9090e3a0449ec5b7461dc
Sophie Kruijt <sophiekruijt@gmail.com>
2023-01-03 20:15:20 -0600

    Configuration moved to AWS Systems Manager

---
+++ aws.py
@@ -0,0 +1,7 @@
+import boto3
+ssm = boto3.client('ssm')
+
+def getApiUrl():
+  parameter = ssm.get_parameter(Name='API_URL', WithDecryption=False)
+  return parameter['Parameter']['Value']
+  
--- program.py
+++ program.py
@@ -2,52 +2,65 @@ import time
 import smbus2
 import requests
 
-fakeData = False
-location = 2
+from aws import getApiUrl
 
-if not fakeData:
-  bus = smbus2.SMBus(1)
-  bus.write_byte(0x40, 0xF5)
+def main():
+    fakeData = True
+    location = 2
 
-  time.sleep(0.3)
+    API_URL = getApiUrl()
+    print("API_URL: %s" % API_URL)
 
-  # SI7021 address, 0x40  Read 2 bytes, Humidity
-  data0 = bus.read_byte(0x40)
-  data1 = bus.read_byte(0x40)
+    if not fakeData:
+        bus = smbus2.SMBus(1)
+        bus.write_byte(0x40, 0xF5)
 
-  # Convert the data
-  humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
+        time.sleep(0.3)
 
-  time.sleep(0.3)
-  bus.write_byte(0x40, 0xF3)
-  time.sleep(0.3)
+        # SI7021 address, 0x40  Read 2 bytes, Humidity
+        data0 = bus.read_byte(0x40)
+        data1 = bus.read_byte(0x40)
 
-  # SI7021 address, 0x40 Read data 2 bytes, Temperature
-  data0 = bus.read_byte(0x40)
-  data1 = bus.read_byte(0x40)
+        # Convert the data
+        humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
 
-  # Convert the data and output it
-  celsTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
-  fahrTemp = celsTemp * 1.8 + 32
+        time.sleep(0.3)
+        bus.write_byte(0x40, 0xF3)
+        time.sleep(0.3)
 
-else:
-  humidity = 90.0
-  fahrTemp = 76.45
-  celsTemp = 24.5
-  
-print("Relative Humidity is : %.2f %%" %humidity)
-print("Temperature in Celsius is : %.2f C" %celsTemp)
-print("Temperature in Fahrenheit is : %.2f F" %fahrTemp)
+        # SI7021 address, 0x40 Read data 2 bytes, Temperature
+        data0 = bus.read_byte(0x40)
+        data1 = bus.read_byte(0x40)
 
-current_time = time.time()
-print(current_time)
-timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-print("Timestamp %s" %timestamp)
+        # Convert the data and output it
+        celsTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
+        fahrTemp = celsTemp * 1.8 + 32
 
-# POST request with SensorMeasurementTypeId 2 (temperature)
-data = { 'LocationId':location, 'value':fahrTemp, 'SensorMeasurementTypeId' : 2, 'timestamp':timestamp}
-r = requests.post("https://api.sophiekruijt.com/v1/measurements", json=data)
+    else:
+        humidity = 90.0
+        fahrTemp = 76.45
+        celsTemp = 24.5
 
-# POST request with SensorMeasurementTypeId 3 (humidity)
-data = { 'LocationId':location, 'value':humidity, 'SensorMeasurementTypeId' : 3, 'timestamp':timestamp}
-r = requests.post("https://api.sophiekruijt.com/v1/measurements", json=data)
+    print("Relative Humidity is : %.2f %%" % humidity)
+    print("Temperature in Celsius is : %.2f C" % celsTemp)
+    print("Temperature in Fahrenheit is : %.2f F" % fahrTemp)
+
+    current_time = time.time()
+    print(current_time)
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+    print("Timestamp %s" % timestamp)
+
+    # POST request with SensorMeasurementTypeId 2 (temperature)
+    print(API_URL + "/v1/measurements")
+ 
+    data = {'LocationId': location, 'value': fahrTemp,
+            'SensorMeasurementTypeId': 2, 'timestamp': timestamp}
+    r = requests.post(API_URL+"/v1/measurements", json=data)
+
+    # POST request with SensorMeasurementTypeId 3 (humidity)
+    data = {'LocationId': location, 'value': humidity,
+            'SensorMeasurementTypeId': 3, 'timestamp': timestamp}
+    r = requests.post(API_URL+"/v1/measurements", json=data)
+
+if __name__ == "__main__":
+    main()
